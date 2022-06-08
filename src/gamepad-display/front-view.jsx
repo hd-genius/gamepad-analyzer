07b62fcf7285213f676b45789952978eb7b6ea07
Roy Stewart <roythomasstewart@gmail.com>
2022-06-08 17:12:43 -0400

    Added a simple front buttons display

--- src/gamepad-display/front-view.jsx
+++ src/gamepad-display/front-view.jsx
@@ -1,3 +1,53 @@
-export const GamepadFrontView = () => {
-    return <svg className="gamepad-display"></svg>;
+import { useGamepad } from "../useGamepad";
+
+export const GamepadFrontView = ({index}) => {
+    const gamepad = useGamepad(index);
+    const bottomFaceButton = gamepad.buttons[0];
+    const rightFaceButton = gamepad.buttons[1];
+    const leftFaceButton = gamepad.buttons[2];
+    const topFaceButton = gamepad.buttons[3];
+
+    const leftBumperButton = gamepad.buttons[4];
+    const rightBumperButton = gamepad.buttons[5];
+
+    const leftTrigger = gamepad.buttons[6];
+    const rightTrigger = gamepad.buttons[7];
+
+    const selectButton = gamepad.buttons[8];
+    const startButton = gamepad.buttons[9];
+
+    const leftStickButton = gamepad.buttons[10];
+    const rightStickButton = gamepad.buttons[11];
+
+    const topDirectionButton = gamepad.buttons[12];
+    const bottomDirectionButton = gamepad.buttons[13];
+    const leftDirectionButton = gamepad.buttons[14];
+    const rightDirectionButton = gamepad.buttons[15];
+
+    return <div style={{
+        display: "flex",
+        "flex-direction": "column"
+    }}>
+        <span>A &ndash; {bottomFaceButton.value}</span>
+        <span>B &ndash; {rightFaceButton.value}</span>
+        <span>X &ndash; {leftFaceButton.value}</span>
+        <span>Y &ndash; {topFaceButton.value}</span>
+
+        <span>LB &ndash; {leftBumperButton.value}</span>
+        <span>RB &ndash; {rightBumperButton.value}</span>
+
+        <span>LT &ndash; {leftTrigger.value}</span>
+        <span>RT &ndash; {rightTrigger.value}</span>
+
+        <span>Select &ndash; {selectButton.value}</span>
+        <span>Start &ndash; {startButton.value}</span>
+
+        <span>Left Stick &ndash; {leftStickButton.value}</span>
+        <span>Right Stick &ndash; {rightStickButton.value}</span>
+
+        <span>&uarr; &ndash; {topDirectionButton.value}</span>
+        <span>&darr; &ndash; {bottomDirectionButton.value}</span>
+        <span>&larr; &ndash; {leftDirectionButton.value}</span>
+        <span>&rarr; &ndash; {rightDirectionButton.value}</span>
+    </div>;
 };
--- src/gamepad-display/gamepad-display.jsx
+++ src/gamepad-display/gamepad-display.jsx
@@ -1,12 +1,13 @@
 import { useGamepad } from "../useGamepad";
 import { GamepadFrontView } from "./front-view";
 import { GamepadTopView } from "./top-view";
+import styles from "./gamepad-display.module.css";
 
 export const GamepadDisplay = ({ index }) => {
     const gamepad = useGamepad(index);
-    return <div>
+    return <div className={styles.gamepadDisplay}>
         <span>Gamepad {index + 1} </span>
-        <GamepadFrontView />
-        <GamepadTopView />
+        <GamepadFrontView index={index} />
+        <GamepadTopView index={index} />
     </div>;
 };
