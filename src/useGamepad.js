5cc728c9f830feb4589af7e1a42fa53100216a10
Roy Stewart <roythomasstewart@gmail.com>
2022-06-08 17:12:12 -0400

    Updated useGamepad to poll for changes

--- src/useGamepad.js
+++ src/useGamepad.js
@@ -1,5 +1,31 @@
 import { useEffect, useState } from "react";
 
 export const useGamepad = (index) => {
-    return navigator.getGamepads()[index];
+    const [lastUpdate, setLastUpdate] = useState(0);
+    const [currentGamepad, setCurrentGamePad] = useState(navigator.getGamepads()[index]);
+
+    useEffect(() => {
+        let shouldPoll = true;
+
+        function startPolling() {
+            const gamepad = navigator.getGamepads()[index];
+
+            if (gamepad.timestamp > lastUpdate) {
+                setCurrentGamePad(gamepad);
+                setLastUpdate(gamepad.timestamp);
+            }
+
+            if (shouldPoll) {
+                window.requestAnimationFrame(startPolling);
+            }
+        };
+
+        const stopPolling = () => shouldPoll = true;
+        
+        window.requestAnimationFrame(startPolling);
+
+        return stopPolling;
+    }, [])
+
+    return currentGamepad;
 };
