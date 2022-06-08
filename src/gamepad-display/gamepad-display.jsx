ee9474c2590d5cb27be84f9da62e72acf38bc3f4
Roy Stewart <roythomasstewart@gmail.com>
2022-06-08 17:52:58 -0400

    Cleaned up GamepadDisplay

--- src/gamepad-display/gamepad-display.jsx
+++ src/gamepad-display/gamepad-display.jsx
@@ -1,12 +1,10 @@
-import { useGamepad } from "../useGamepad";
 import { GamepadFrontView } from "./front-view";
 import { GamepadTopView } from "./top-view";
 import styles from "./gamepad-display.module.css";
 
 export const GamepadDisplay = ({ index }) => {
-    const gamepad = useGamepad(index);
     return <div className={styles.gamepadDisplay}>
-        <span>Gamepad {index + 1} </span>
+        <span>Gamepad {index + 1}</span>
         <GamepadFrontView index={index} />
         <GamepadTopView index={index} />
     </div>;
