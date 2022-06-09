cf2717697e453d69e6cf5ddf047e855b4ab2240e
Roy Stewart <roythomasstewart@gmail.com>
2022-06-08 22:23:41 -0400

    Converted the front display to use the image

--- src/gamepad-display/front-view.jsx
+++ src/gamepad-display/front-view.jsx
@@ -1,6 +1,30 @@
 import { useGamepad } from "../useGamepad";
 
-export const GamepadFrontView = ({index}) => {
+const blackOutline = {
+    stroke: "#000000",
+    strokeWidth: "0.264583px",
+    strokeLinecap: "butt",
+    strokeLinejoin: "miter",
+    strokeDasharray: "none",
+    strokeOpacity: 1,
+};
+
+const filled = {
+    fill: "#ffffff",
+    fillOpacity: 1,
+};
+
+const empty = {
+    fill: "none",
+    fillOpacity: 1,
+};
+
+const pressedStyles = {
+    fill: "red",
+    fillOpacity: 1,
+};
+
+export const GamepadFrontView = ({ index }) => {
     const gamepad = useGamepad(index);
 
     const bottomFaceButton = gamepad.buttons[0];
@@ -25,28 +49,121 @@ export const GamepadFrontView = ({index}) => {
     const rightHorizontalAxis = gamepad.axes[2];
     const rightVerticalAxis = gamepad.axes[3];
 
-    return <div style={{
-        display: "flex",
-        flexDirection: "column"
-    }}>
-        <span>A &ndash; {bottomFaceButton.value}</span>
-        <span>B &ndash; {rightFaceButton.value}</span>
-        <span>X &ndash; {leftFaceButton.value}</span>
-        <span>Y &ndash; {topFaceButton.value}</span>
-
-        <span>Select &ndash; {selectButton.value}</span>
-        <span>Start &ndash; {startButton.value}</span>
-
-        <span>Left Stick &ndash; {leftStickButton.value}</span>
-        <span>Right Stick &ndash; {rightStickButton.value}</span>
-
-        <span>&uarr; &ndash; {topDirectionButton.value}</span>
-        <span>&darr; &ndash; {bottomDirectionButton.value}</span>
-        <span>&larr; &ndash; {leftDirectionButton.value}</span>
-        <span>&rarr; &ndash; {rightDirectionButton.value}</span>
-
-        <span>Left &ndash; {leftHorizontalAxis}, {leftVerticalAxis}</span>
-
-        <span>Right &ndash; {rightHorizontalAxis}, {rightVerticalAxis}</span>
-    </div>;
+    return <svg
+        width="66.03643mm"
+        height="46.065399mm"
+        viewBox="0 0 66.03643 46.065399"
+        version="1.1"
+        id="svg5">
+        <defs
+            id="defs2" />
+        <g
+            id="layer1"
+            transform="translate(-0.48021528,-0.47886159)">
+            <use
+                x="0"
+                y="0"
+                href="#path6268"
+                id="use8692"
+                transform="matrix(-1,0,0,1,67.233916,-3.4567645e-6)"
+                width="100%"
+                height="100%" />
+            <path
+                style={{
+                    ...filled,
+                    ...blackOutline,
+                }}
+                d="m 42.587711,2.0034724 c 0,0 1.98244,-1.15648003 3.02612,-1.39229003 1.73339,-0.003 3.5864,0.25464 5.52312,1.00012003 2.3768,0.91488 3.70387,1.6771 4.38492,2.2889 0.67422,0.87956 1.03354,2.99724 1.03354,2.99724 l -13.98597,-3.92564 z"
+                id="path6268" />
+            <rect
+                style={{
+                    ...filled,
+                    ...blackOutline,
+                }}
+                id="rect5253"
+                width="17.941517"
+                height="1.5712559"
+                x="24.646204"
+                y="2.0034697" />
+            <path
+                style={{
+                    ...filled,
+                    ...blackOutline,
+                }}
+                d="m 92.222656,105.9375 c -1.66807,-0.1117 -2.635128,0.0322 -6.355468,1.39453 -3.72034,1.3624 -3.876934,2.173 -5.220704,3.56641 -2.45975,2.55062 -6.5625,17.67187 -6.5625,17.67187 0,0 -2.027115,7.18724 -1.859375,10.85352 0.1556,3.4009 0.05235,4.86406 2.382813,7.66797 1.05904,1.27421 4.126953,2.69726 4.126953,2.69726 0,0 1.078116,0.29028 5.072266,-4.68945 3.99415,-4.97973 6.875355,-8.24321 8.984375,-8.2832 l 12.308594,-0.0332 12.30859,0.0332 c 2.10902,0.04 4.99023,3.30347 8.98438,8.2832 3.99415,4.97973 5.07226,4.68945 5.07226,4.68945 0,0 3.06792,-1.42305 4.12696,-2.69726 2.33046,-2.80391 2.22916,-4.26707 2.38476,-7.66797 0.16774,-3.66628 -1.86133,-10.85352 -1.86133,-10.85352 0,0 -4.10275,-15.12125 -6.5625,-17.67187 -1.34377,-1.39341 -1.49841,-2.20401 -5.21875,-3.56641 -3.72034,-1.36235 -4.68935,-1.50623 -6.35742,-1.39453 -1.66808,0.1117 -3.22461,0.51562 -3.22461,0.51562 l -9.65234,-0.0762 -9.652344,0.0762 c 0,0 -1.55653,-0.40392 -3.22461,-0.51562 z"
+                id="path54"
+                transform="translate(-71.602149,-103.38007)"
+                class="UnoptimicedTransforms" />
+            <HomeButton x="33.563824" y="6.4840302" />
+            <FaceButton x="50.772846" y="8.8731289" pressed={topFaceButton.pressed} />
+            <FaceButton x="55.2308185" y="13.4041824" pressed={rightFaceButton.pressed} />
+            <FaceButton x="46.1687105" y="13.3311024" pressed={leftFaceButton.pressed} />
+            <FaceButton x="50.62668249" y="17.4967424" pressed={bottomFaceButton.pressed} />
+            <Joystick x="16.753397" y="13.148405" pressed={leftStickButton.pressed} />
+            <Joystick x="42.405007" y="23.160568" pressed={rightStickButton.pressed} />
+            <MenuButton x="28.90851" y="13.150282" pressed={selectButton.pressed} />
+            <MenuButton x="38.5203265" y="13.150282" pressed={startButton.pressed} />
+            <path
+                style={{
+                    ...empty,
+                    ...blackOutline,
+                }}
+                d="m 26.561666,19.093082 v 3.10058 h 3.094325 c 0,0 0.32886,0.28236 0.32886,1.62464 0,1.34228 -0.32886,1.786 -0.32886,1.786 H 26.47972 v 3.10059 c 0,0 -0.492226,0.19379 -1.498617,0.19379 -1.00639,0 -1.550294,-0.19379 -1.550294,-0.19379 v -3.10059 h -3.307292 c 0,0 -0.354629,-0.25926 -0.356003,-1.71605 -0.0014,-1.45678 0.356003,-1.68519 0.356003,-1.68519 h 3.307292 v -3.10999 c 0,0 0.1609,-0.29232 1.595937,-0.29232 1.435036,0 1.53492,0.29233 1.53492,0.29233 z"
+                id="path3582" />
+        </g>
+    </svg>;
 };
+
+const HomeButton = ({ x, y }) => {
+    return <ellipse
+        style={{
+            ...empty,
+            ...blackOutline,
+        }}
+        cx={x}
+        cy={y}
+        rx="2.4029541"
+        ry="2.2737632" />
+}
+
+const FaceButton = ({ pressed, x, y }) => {
+    const statusStyles = pressed ? pressedStyles : {};
+    return <ellipse
+        style={{
+            ...empty,
+            ...blackOutline,
+            ...statusStyles,
+        }}
+        cx={x}
+        cy={y}
+        rx="2.1559045"
+        ry="2.1193638" />;
+};
+
+const MenuButton = ({ pressed, x, y }) => {
+    const statusStyles = pressed ? pressedStyles : {};
+    return <circle
+        style={{
+        ...empty,
+        ...blackOutline,
+        ...statusStyles,
+        }}
+        cx={x}
+        cy={y}
+        r="1.2919108" />;
+}
+
+const Joystick = ({ pressed, x, y, horizontal, vertical }) => {
+    // TODO: add position display logic
+    const statusStyles = pressed ? pressedStyles : {};
+    return <ellipse
+        style={{
+            ...empty,
+            ...blackOutline,
+            ...statusStyles,
+        }}
+        cx={x}
+        cy={y}
+        rx="3.4348307"
+        ry="3.5444534" />;
+}
