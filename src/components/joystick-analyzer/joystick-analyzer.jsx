970d6d035b92a36d3e7a7a1565455d1acad49bb0
Roy Stewart <roythomasstewart@gmail.com>
2022-06-12 16:05:52 -0400

    Added the display logic for JoystickAnalyzer

--- src/components/joystick-analyzer/joystick-analyzer.jsx
+++ src/components/joystick-analyzer/joystick-analyzer.jsx
@@ -1,3 +1,13 @@
+import { LEFT, RIGHT } from "../../constants";
+import { useJoystick } from "../../hooks";
+import { Joystick } from "./joystick";
+
 export const JoystickAnalyzer = () => {
-    return <span>Joystick Analyzer</span>;
+    const leftJoystick = useJoystick(LEFT);
+    const rightJoystick = useJoystick(RIGHT);
+
+    return <div>
+        <Joystick horizontal={leftJoystick.x} vertical={leftJoystick.y} />
+        <Joystick horizontal={rightJoystick.x} vertical={rightJoystick.y} />
+    </div>;
 };
---
+++ src/components/joystick-analyzer/joystick.jsx
@@ -0,0 +1,11 @@
+import styles from "./joystick.module.css";
+
+export const Joystick = ({ horizontal, vertical }) => {
+    const movementScale = 50;
+    return <svg viewBox="0 0 100 100" width="12em">
+        <circle cx={50} cy={50} className={styles.outlined} r={50} />
+        <line className={styles.outlined} x1={0} x2={100} y1={50} y2={50} />
+        <line className={styles.outlined} x1={50} x2={50} y1={0} y2={100} />
+        <circle cx={50 + movementScale * horizontal} cy={50 + movementScale * vertical} className={styles.outlined} r={4} />
+    </svg>;
+};
