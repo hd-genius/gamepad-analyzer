58c271f738333f04fed5971b02f3b9e6ce121040
Roy Stewart <roythomasstewart@gmail.com>
2022-06-13 07:35:12 -0400

    Added basic display for the trigger analyzer

--- src/components/trigger-analyzer/trigger-analyzer.jsx
+++ src/components/trigger-analyzer/trigger-analyzer.jsx
@@ -1,3 +1,18 @@
+import { LEFT_TRIGGER, RIGHT_TRIGGER } from "../../constants";
+import { useButton } from "../../hooks";
+
 export const TriggerAnalyzer = () => {
-    return <span>Trigger Analyzer</span>;
+    const leftTrigger = useButton(LEFT_TRIGGER);
+    const rightTrigger = useButton(RIGHT_TRIGGER);
+    return <div>
+        <Trigger name="Left Trigger" value={leftTrigger.value} />
+        <Trigger name="Right Trigger" value={rightTrigger.value} />
+    </div>;
+};
+
+const Trigger = ({ name, value }) => {
+    return <div>
+        <span>{name}</span>
+        <span>{value}</span>
+    </div>;
 };
