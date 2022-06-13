d552b536405de2306745033eb05df89bacb8410d
Roy Stewart <roythomasstewart@gmail.com>
2022-06-13 17:05:04 -0400

    Fixed broken test suites

--- src/components/joystick-analyzer/joystick-analyzer.test.jsx
+++ src/components/joystick-analyzer/joystick-analyzer.test.jsx
@@ -1,3 +1,7 @@
 describe("JoystickAnalyzer", () => {
-    //
+    describe("values table", () => {
+        it.todo("should show the horizontal value of the left joystick");
+
+        it.todo("should show the horizontal value of the right joystick");
+    })
 });
