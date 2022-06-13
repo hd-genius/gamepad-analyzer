1a66aa5f0a9bd8ae793ae8271588f7cc5d57a073
Roy Stewart <roythomasstewart@gmail.com>
2022-06-13 16:55:29 -0400

    Added tests for useJoystick

---
+++ src/hooks/useJoystick.test.js
@@ -0,0 +1,26 @@
+import { useJoystick } from "./useJoystick";
+import { useGamepad } from "./useGamepad";
+
+jest.mock("./useGamepad");
+
+describe("useJoystick", () => {
+    it.each([
+        ["left", 0, 0.56, 0.56, 0.12, 0.34, 0.89],
+        ["right", 1, 0.34, 0.56, 0.12, 0.34, 0.89]
+    ])("should return the correct value for the %s joystick's horizontal axis", (name, joystickId, expectedValue, ...axes) => {
+        useGamepad.mockReturnValue({
+            axes
+        });
+        expect(useJoystick(joystickId).x).toEqual(expectedValue);
+    });
+
+    it.each([
+        ["left", 0, 0.12, 0.56, 0.12, 0.34, 0.89],
+        ["right", 1, 0.89, 0.56, 0.12, 0.34, 0.89]
+    ])("should return the correct value for the %s joystick's vertical axis", (name, joystickId, expectedValue, ...axes) => {
+        useGamepad.mockReturnValue({
+            axes
+        });
+        expect(useJoystick(joystickId).y).toEqual(expectedValue);
+    });
+});
