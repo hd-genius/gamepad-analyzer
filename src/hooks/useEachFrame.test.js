0798bef7de4bcdc4e3260cf23fea526fdceeab68
Roy Stewart <roythomasstewart@gmail.com>
2022-06-14 20:40:34 -0400

    Added another test for useEachFrame

--- src/hooks/useEachFrame.test.js
+++ src/hooks/useEachFrame.test.js
@@ -15,6 +15,14 @@ describe("useEachFrame", () => {
         expect(window.requestAnimationFrame).toHaveBeenCalled();
     });
 
+    it("should call the callback during the next animation frame", () => {
+        const callback = jest.fn();
+        renderHook(() => useEachFrame(callback));
+        const nextFrameCallback = window.requestAnimationFrame.mock.calls[0][0];
+        nextFrameCallback();
+        expect(callback).toHaveBeenCalled();
+    });
+
     it("should queue the callback for the next animation frame after it has been called", () => {
         renderHook(() => useEachFrame(() => {}));
         const nextFrameCallback = window.requestAnimationFrame.mock.calls[0][0];
