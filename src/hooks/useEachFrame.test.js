bd7ab62ac70643b707dcd1491639805999614649
Roy Stewart <roythomasstewart@gmail.com>
2022-06-14 20:38:34 -0400

    Added tests for useEachFrame

--- src/hooks/useEachFrame.test.js
+++ src/hooks/useEachFrame.test.js
@@ -1,7 +1,34 @@
+import { useEachFrame } from "./useEachFrame";
+import { renderHook } from "@testing-library/react-hooks"
+
 describe("useEachFrame", () => {
-    it.todo("should queue the callback for the next animation frame");
+    beforeAll(() => {
+        jest.spyOn(window, "requestAnimationFrame");
+    });
 
-    it.todo("should queue the callback for the next animation frame after it has been called");
+    beforeEach(() => {
+        window.requestAnimationFrame.mockReset();
+    });
 
-    it.todo("should not queue the callback for the next animation frame once the effect is cleaned up");
+    it("should queue the callback for the next animation frame", () => {
+        renderHook(() => useEachFrame(() => {}));
+        expect(window.requestAnimationFrame).toHaveBeenCalled();
+    });
+
+    it("should queue the callback for the next animation frame after it has been called", () => {
+        renderHook(() => useEachFrame(() => {}));
+        const nextFrameCallback = window.requestAnimationFrame.mock.calls[0][0];
+        window.requestAnimationFrame.mockReset();
+        nextFrameCallback();
+        expect(window.requestAnimationFrame).toHaveBeenCalled();
+    });
+
+    it("should not queue the callback for the next animation frame once the effect is cleaned up", () => {
+        const { unmount } = renderHook(() => useEachFrame(() => {}));
+        const nextFrameCallback = window.requestAnimationFrame.mock.calls[0][0];
+        window.requestAnimationFrame.mockReset();
+        unmount();
+        nextFrameCallback();
+        expect(window.requestAnimationFrame).not.toHaveBeenCalled();
+    });
 });
