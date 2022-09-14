db48d0e5f70f1e7b50af5e5158236f396e101d88
Roy Stewart <roythomasstewart@gmail.com>
2022-09-14 19:05:42 -0400

    Corrected dependency issues

--- src/hooks/useEachFrame.test.js
+++ src/hooks/useEachFrame.test.js
@@ -1,5 +1,5 @@
 import { useEachFrame } from "./useEachFrame";
-import { renderHook } from "@testing-library/react-hooks";
+import { renderHook } from "@testing-library/react";
 
 describe("useEachFrame", () => {
     beforeAll(() => {
