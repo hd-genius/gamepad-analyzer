c871d81c342fc090c6de23c9cb8fb7a2c7e99c07
Roy Stewart <roythomasstewart@gmail.com>
2025-06-18 23:24:30 -0400

    Centered the "no gamepads" text

--- src/components/status-indicator/status-indicator.jsx
+++ src/components/status-indicator/status-indicator.jsx
@@ -1,3 +1,5 @@
+import styles from "./status-indicator.module.css";
+
 export const StatusIndicator = ({ children, ...props }) => {
-    return <div {...props}>{children}</div>;
+    return <div className={styles.statusIndicator} {...props}>{children}</div>;
 };
