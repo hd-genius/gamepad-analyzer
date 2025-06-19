48ab1a84ca71f61c07a5275c0953bace32e0934e
Roy Stewart <roythomasstewart@gmail.com>
2025-06-19 07:33:06 -0400

    Added the color change for dpad button presses

--- src/components/analyzers/basic-analyzer/front-view/directional-pad.tsx
+++ src/components/analyzers/basic-analyzer/front-view/directional-pad.tsx
@@ -2,6 +2,31 @@ import classNames from "classnames";
 import drawingStyles from "../../drawing.module.css";
 import { ButtonProps } from "../button-props";
 
+class Vector2D {
+    x: number;
+    y: number;
+
+    constructor(x: number, y: number) {
+        this.x = x;
+        this.y = y;
+    }
+
+    add(vector: Vector2D): Vector2D {
+        return new Vector2D(this.x + vector.x, this.y + vector.y);
+    }
+
+    toString(): string {
+        return `${this.x},${this.y}`;
+    }
+}
+
+enum Direction {
+    UP,
+    RIGHT,
+    DOWN,
+    LEFT,
+}
+
 interface DirectionalPadProps {
     up: boolean;
     right: boolean;
@@ -17,44 +42,39 @@ export const DirectionalPad = ({
 }: DirectionalPadProps) => {
     return (
         <>
+            <defs>
+                <clipPath id="dpad-clip-boundary">
+                    <use href="#dpad-outline"  />
+                </clipPath>
+            </defs>
+            <g clipPath="url(#dpad-clip-boundary)">
+                <DirectionalPadButton pressed={up} direction={Direction.UP} />
+                <DirectionalPadButton pressed={right} direction={Direction.RIGHT} />
+                <DirectionalPadButton pressed={down} direction={Direction.DOWN} />
+                <DirectionalPadButton pressed={left} direction={Direction.LEFT} />
+            </g>
             <path
-                className={drawingStyles.outlined}
+                id="dpad-outline"
+                className={classNames(drawingStyles.outlined, drawingStyles.empty)}
                 d="m 26.56,19.09 v 3.1 h 3.1 c 0,0 0.33,0.28 0.33,1.62 0,1.34 -0.33,1.79 -0.33,1.79 H 26.48 v 3.1 c 0,0 -0.49,0.19 -1.5,0.19 -1,0 -1.55,-0.19 -1.55,-0.19 v -3.1 h -3.31 c 0,0 -0.35,-0.26 -0.36,-1.72 0,-1.46 0.36,-1.69 0.36,-1.69 h 3.31 v -3.11 c 0,0 0.16,-0.29 1.6,-0.29 1.44,0 1.53,0.29 1.53,0.29 z"
             />
-            {/* <DirectionalPadButton pressed={up} direction={Direction.UP} />
-            <DirectionalPadButton pressed={right} direction={Direction.RIGHT} />
-            <DirectionalPadButton pressed={down} direction={Direction.DOWN} />
-            <DirectionalPadButton pressed={left} direction={Direction.LEFT} /> */}
         </>
     );
 };
 
-const DIRECTIONAL_PAD_CENTER = {
-    x: 0,
-    y: 0,
-};
+const DIRECTIONAL_PAD_CENTER = new Vector2D(25.06, 24.09);
 
-enum Direction {
-    UP,
-    RIGHT,
-    DOWN,
-    LEFT,
+const dpadRadius = 6;
+
+const ROTATION_MAP = {
+    [Direction.UP]: 0,
+    [Direction.RIGHT]: 90,
+    [Direction.DOWN]: 180,
+    [Direction.LEFT]: -90
 }
 
-const rotationForDirection = (direction: Direction) => {
-    switch (direction) {
-        case Direction.UP:
-            return 0;
-        case Direction.RIGHT:
-            return 90;
-        case Direction.DOWN:
-            return 180;
-        case Direction.LEFT:
-            return -90;
-    }
-};
-
 interface DirectionalPadButtonProps extends ButtonProps {
+    pressed: boolean;
     direction: Direction;
 }
 
@@ -62,11 +82,14 @@ const DirectionalPadButton = ({
     pressed,
     direction,
 }: DirectionalPadButtonProps) => {
-    const rotation = rotationForDirection(direction);
+    const rotation = ROTATION_MAP[direction];
+    const upperLeft = new Vector2D(-dpadRadius, -dpadRadius);
+
     return (
         <path
-            className={classNames(drawingStyles.pressed)}
-            d="m 26.561666,19.093082 v 3.10058 h 3.094325 c 0,0 0.32886,0.28236 0.32886,1.62464 0,1.34228 -0.32886,1.786 -0.32886,1.786 H 26.47972 v 3.10059 c 0,0 -0.492226,0.19379 -1.498617,0.19379 -1.00639,0 -1.550294,-0.19379 -1.550294,-0.19379 v -3.10059 h -3.307292 c 0,0 -0.354629,-0.25926 -0.356003,-1.71605 -0.0014,-1.45678 0.356003,-1.68519 0.356003,-1.68519 h 3.307292 v -3.10999 c 0,0 0.1609,-0.29232 1.595937,-0.29232 1.435036,0 1.53492,0.29233 1.53492,0.29233 z"
+            className={classNames({[drawingStyles.pressed]: pressed})}
+            d={`m ${DIRECTIONAL_PAD_CENTER} l ${upperLeft} h ${dpadRadius * 2} z`}
+            transform={`rotate(${rotation} ${DIRECTIONAL_PAD_CENTER.x} ${DIRECTIONAL_PAD_CENTER.y})`}
         />
     );
 };
