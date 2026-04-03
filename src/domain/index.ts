432b79a4886e8a95c9f7cebdb22910822a65a82a
Roy Stewart <roythomasstewart.com>
2026-04-03 19:56:20 -0400

    fix the type export for VibrationActuator

--- src/domain/index.ts
+++ src/domain/index.ts
@@ -1,5 +1,7 @@
+import * as vibMod from "./vibrationActuator"
+console.log(JSON.stringify(vibMod))
 export { Analyzers } from "./analyzers";
 export { Buttons } from "./buttons";
 export { ControllerType } from "./controller-type";
 export { Joysticks } from "./joysticks";
-export { VibrationActuatorType, VibrationActuator } from "./vibrationActuator"
+export { VibrationActuatorType, type VibrationActuator } from "./vibrationActuator";
